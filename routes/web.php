498da153e9f806166db21217578240b7263b6fc4
nabilwafi <nabilwafi88@gmail.com>
2025-08-03 19:54:37 +0700

    feat: adding dashboard file and client file

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 require __DIR__.'/settings.php';
+require __DIR__.'/dashboard.php';
+require __DIR__.'/client.php';
 require __DIR__.'/auth.php';
